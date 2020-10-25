c29652b79e1fed2ed67e72c9208fca91b79a6a60
WarBall <desik_t9@mail.ru>
2020-10-25 23:32:44 +0300

    Добавьте файлы проекта.

---
+++ FirstCode-Lab3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCode_Lab3
+{
+    class Program
+    {
+        /*
+    * Данный метод выводит пользователю приглашение,
+    * указанное в аргументе anOutputStr, получает введенную
+    * с клавиатуры строку, преобразует её в целое число и
+    * возвращает это число
+    */
+        static int GetNumber(string Str)
+        {
+            //Вывод приглашения пользователю
+            Console.Write(Str);
+
+            //Получение строки
+            string tmpString = Console.ReadLine();
+
+            //Преобразование строки в число
+            int tmpArg = Convert.ToInt32(tmpString);
+
+            return tmpArg;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, c, max;
+
+            try
+            {
+
+                a = GetNumber("Введите число а:");
+                b = GetNumber("Введите число b:");
+                c = GetNumber("Введите число c:");
+
+                if (a > b)
+                {
+                    if(a > c)
+                    {
+                        max = a;
+                    }
+                else
+                {
+                    max = c;
+                }
+                }
+                else
+                {
+                    if(b > c)
+                    {
+                        max = b;
+                    }
+                    else
+                    {
+                        max = c;
+                    }
+            }
+                Console.WriteLine("MAX = " + max);
+
+            }
+            catch (SystemException)
+            {
+                Console.WriteLine("Произошла ошибка, вероятно, были введены некорректные данные!");
+            }
+        }
+    }
+}
